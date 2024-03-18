d21a2becbc564f45e4ffa7beb9fff252a01f9ce7
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-03-18 00:20:27 -0300

    tela-cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'about', component: AboutComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent}
 ];
 
 @NgModule({
