bc2caf1ca013619df41db3a3552a15f67ce3649a
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-03-18 20:24:18 -0300

    Tela Cadastro Médico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DoctorLoginComponent } from './components/doctor-login/doctor-login.component';
+import { DoctorRegisterComponent } from './components/doctor-register/doctor-register.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'about', component: AboutComponent},
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
-  {path:'doctorlogin', component: DoctorLoginComponent}
+  {path:'doctorlogin', component: DoctorLoginComponent},
+  {path:'doctorregister', component: DoctorRegisterComponent},
 ];
 
 @NgModule({
