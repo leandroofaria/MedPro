03778b2c9a47be8adcde485b865ad7303729a630
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2024-05-25 19:27:43 -0300

    Header Responsivo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
-}
+  menuValue:boolean=false;
+  menu_icon :string ='bi bi-list';
+  openMenu(){
+     this.menuValue =! this.menuValue ;
+     this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+   }
+    closeMenu() {
+     this.menuValue = false;
+     this.menu_icon = 'bi bi-list';
+   }
+  }
